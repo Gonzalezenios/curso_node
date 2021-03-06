716479b441e6a7c9a77db64cb1711ebe5478d971
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-03-06 13:39:59 -0300

    diferentes maneras de definir las variables en Javascript

---
+++ const-let-var.js
@@ -0,0 +1,34 @@
+//Variables usando const
+
+const nombre = 'Enio'
+
+if (true) {
+    const nombre = 'Maria';
+    console.log(nombre);
+}
+
+console.log(nombre);
+
+
+//Variables usando let
+
+let apellido = 'Gonzalez'
+
+if (true) {
+    let apellido = 'Araujo'
+    console.log(apellido);
+}
+
+console.log(apellido);
+
+
+// Variables usando var
+
+var años = 31
+
+if (true) {
+    var edad = 25
+    console.log(edad);
+}
+
+console.log(años);
---
+++ templateString.js
@@ -0,0 +1,9 @@
+const  nombre = 'Francisco'
+const apodo = 'Francis'
+
+const salida = nombre + ' ' + apodo
+
+const templateString = `${nombre} ${apodo}`
+
+console.log(salida);
+console.log(templateString);
