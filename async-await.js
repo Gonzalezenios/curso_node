2f956365a3855ea42b48b9437d8053144df1a339
Enio Gonzalez <gonzalezeniood@gmail.com>
2021-03-07 22:40:18 -0300

    funciones con asyn await

--- async-await.js
+++ async-await.js
@@ -1,37 +1,71 @@
-
 const empleados = [{
-    id: 1,
-    nombre: "Juana",
-},
-{
-    id: 2,
-    nombre: "Luis",
-},
-{
-    id: 3,
-    nombre: "Laura",
-},
+        id: 1,
+        nombre: "Juana",
+    },
+    {
+        id: 2,
+        nombre: "Luis",
+    },
+    {
+        id: 3,
+        nombre: "Laura",
+    },
 ];
 
 const sueldos = [{
-    id: 1,
-    sueldo: 1500,
-},
-{
-    id: 2,
-    sueldo: 2000,
-},
+        id: 1,
+        sueldo: 1500,
+    },
+    {
+        id: 2,
+        sueldo: 2000,
+    },
 ];
 
+const getSueldoById = async (id) => {
+
+    try {
+        const sueldo = sueldos.find((e) => e.id === id);
+
+        if (sueldo) {
+            return sueldo;
+        }
+        throw new Error(`El sueldo con id ${id} no existe`)
+    } catch (error) {
+        throw error;
+
+    }
+}
+
 const getEmpleadoById = async (id) => {
-    return new Promise((resolve, reject) => {
+    try {
         const empleado = empleados.find((e) => e.id === id);
 
         if (empleado) {
-            resolve(empleado);
-            return;
+            return empleado;
         }
-        reject(`El empleado con id ${id} no existe`);
-    });
+        throw new Error(`El empleado con id ${id} no existe`)
+    } catch (error) {
+        throw error;
+    }
 };
 
+const id = 5;
+
+const getDatosCompletosEmpleado = async (id) => {
+
+    try {
+
+        const empleado = await getEmpleadoById(id);
+        const sueldo = await getSueldoById(id);
+        return `El empleado con id ${id} es ${empleado.nombre} y tiene un sueldo de ${sueldo.sueldo}`;
+
+    } catch (err) {
+        throw err;
+    }
+
+}
+
+getDatosCompletosEmpleado(id)
+    .then(mensaje => {console.log(mensaje)})
+    .catch(err => console.log(err.message));
